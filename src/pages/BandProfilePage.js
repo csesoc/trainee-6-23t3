1e36fa90b4cbf48610665347343eff00b9fa2f92
Nelson Lin <nelson.lin2308@gmail.com>
2023-12-10 16:31:14 +1100

    changes to navbar and to bandprofilepage

--- src/pages/BandProfilePage.js
+++ src/pages/BandProfilePage.js
@@ -1,77 +1,42 @@
 import React from "react";
 import Navbar from "./Navbar.js";
+import "../BandProfilePage.css";
 
-const BandProfilePage = () => {
-    // purple 94618E dark purple 49274A white F8EEE7 cream F4DECB 
-    document.body.style.background = 'linear-gradient(90deg, #ad8df7, #f0a1b9)';
-    document.body.style.margin = "0";
-
-    const container = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "120vh",
-        // border: "5px solid orange"
-    }
-
-    const column = {
-        // border: "5px solid green",
-        marginTop: "40px",
-    }
-
-    const block = {
-        backgroundColor: "#F8EEE7",
-        color: "#49274A",
-        padding: "20px",
-        margin: "10px",
-        borderRadius: "10px",
-    }
-
-    const ul = {
-        listStyleType: "none",
-        margin: "0",
-        padding: "0",
-    }
-
-    const li = {
-        backgroundColor: "#F4DECB",
-        margin: "10px",
-        padding: "10px",
-        color: "#49274A",
-        borderRadius: "10px",
-    }
+const BandProfilePage = ({image, bandName, memberList, performanceList, information}) => {
+    // purple 94618E dark purple 49274A white F8EEE7 cream F4DECB
 
     return (
         <div>
             <Navbar />
-            <div className="band-container" style={container}>
-                <div className="column" style={{...column, width: "300px"}}>
-                    <div className="band-block" style={{...block, display: "flex", alignItems: "center", flexDirection: "column"}}>
-                        <img src={require("./defaultImage.png")} />
-                        <h1>Band Name</h1>
+            <div className="container">
+                <div className="column">
+                    <div className="block-centered">
+                        { image ? <img src={require("./" + image)} /> : <img src={require("./defaultImage.png")} /> }
+                        <h1>{ bandName !== undefined ? bandName : "Band Name" }</h1>
                         <h2>Socials</h2>
+                        <div style={{display: "flex"}}>
+                            <img src={require("./twitter.png")} style={{width: "40px", margin: "0 20px"}} />
+                            <img src={require("./facebook.png")} style={{width: "40px", margin: "0 20px"}} />
+                            <img src={require("./instagram.png")} style={{width: "40px", margin: "0 20px"}} />
+                        </div>
                     </div>
-                    <div className="band-block" style={block}>
+                    <div className="block">
                         <h1>Band Members</h1>
-                        <ul style={ul}>
-                            <li style={li}>Band Member 1</li>
-                            <li style={li}>Band Member 2</li>
-                            <li style={li}>Band Member 3</li>
+                        <ul>
+                            { memberList ? memberList.map((item, index) => (<li key={index}>{item}</li>)) : <li>No Members</li> }
                         </ul>
                     </div>
                 </div>
-                <div className="column" style={column}>
-                    <div className="band-block" style={block}>
+                <div className="column">
+                    <div className="block">
                         <h1>Upcoming Performances</h1>
-                        <ul style={ul}>
-                            <li style={li}>Performance 1</li>
-                            <li style={li}>Performance 2</li>
-                            <li style={li}>Performance 3</li>
+                        <ul>
+                            { performanceList ? performanceList.map((item, index) => (<li key={index}>{item}</li>)) : <li>No Performances</li> }
                         </ul>
                     </div>
-                    <div className="band-block" style={block}>
+                    <div className="block">
                         <h1>Band Information</h1>
-                        <p>Hello world</p>
+                        <p>{ information !== undefined ? information : "Band Information" }</p>
                     </div>
                 </div>
             </div>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,34 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../Navbar.css";
 
 const Navbar = () => {
     const navigate = useNavigate();
 
-    const navbar = {
-        // backgroundColor: "#49274A",
-        margin: "10px 0",
-        padding: "0 10%",
-        display: "flex",
-        justifyContent: "space-between",
-    }
-
-    const element = {
-        // backgroundColor: "#94618E",
-        border: "1px solid #49274A",
-        color: "49274A",
-        padding: "20px",
-        display: "flex",
-        alignItems: "center",
-        borderRadius: "10px",
-        margin: "10px",
-    }
-
     return (
-        <div style={navbar}>
-            <div style={element} onClick={() => navigate('/')}>Home</div>
-            <div style={element} onClick={() => navigate('/login')}>Login</div>
-            <div style={element} onClick={() => navigate('/venues')}>Book Venues</div>
-            <div style={element} onClick={() => navigate('/search')}>Find Gigs</div>
+        <div className='navbar'>
+            <div className='element' onClick={() => navigate('/')}>Home</div>
+            <div className='element' onClick={() => navigate('/login')}>Login</div>
+            <div className='element' onClick={() => navigate('/venues')}>Book Venues</div>
+            <div className='element' onClick={() => navigate('/search')}>Find Gigs</div>
         </div>
     )
 }
