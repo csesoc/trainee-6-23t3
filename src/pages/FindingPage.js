f9c59347b3f442bb7da41944478f59df8a984cdf
issac <issac.arjs@gmail.com>
2023-12-12 17:03:34 +1100

    issac's branch

---
+++ src/pages/FindingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const description = {
+  // Your style properties go here
+};
+
+const FindingPage = () => {
+  return (
+    <div style={description}>
+      findingPagendjwndj
+    </div>
+  );
+};
+
+export default FindingPage;
